aa6d868042bd2610ae5ca79233ed021fac5f532e
kh-ibragim <ibragim2010@list.ru>
2017-07-18 06:42:49 -0500

    profile

---
+++ pages/profile.vue
@@ -0,0 +1,318 @@
+<template>
+<div>
+
+
+
+<div class="uk-section uk-section-muted uk-section uk-padding-remove-top uk-padding-remove-bottom ">
+ <div class="uk-card uk-card-muted uk-card-small uk-grid-collapse  uk-margin  uk-width-1-1 sect uk-margin-remove" data-uk-grid>
+    
+    <div class="uk-card-media-right uk-overflow-hidden uk-width-1-5@s ">
+        <div class="avatar">
+            <img  class="uk-border-circle"  src="http://www.hausmodels.com.au/wp-content/uploads/2015/10/Emily-T-Model-Square.jpg" alt=""/>
+        </div>
+    </div>
+    
+
+   </div>
+
+
+
+
+
+
+
+
+<!--
+{/*primary info*/}-->
+
+  <div class="uk-section uk-section-default uk-padding-remove ">       
+
+
+
+
+
+        <!--{/*navigation tabs*/}-->
+
+        <ul class="uk-flex-center uk-margin-remove uk-background-muted" data-uk-tab>
+            <li class="uk-active"><a href="">Life</a></li>
+            <li><a href="">Meetings</a></li>
+            <li><a href="">My Posts</a></li>
+            <li><a href="">Messages</a></li>
+        </ul>
+
+
+
+
+
+<ul class="uk-switcher uk-margin-remove uk-padding-remove">
+
+<li class="uk-active">
+<!--{/*life*/}-->
+<div  class="uk-margin-remove" >
+    <div class="uk-width-1-1@s uk-margin-remove">
+        <div >
+            <Maps />
+        </div>
+    </div>
+</div>
+</li>
+
+<li>
+<!--{/*meetings*/}-->
+    <div  class=" uk-child-width-1-3@l uk-child-width-1-3@m uk-child-width-1-1@s uk-grid-medium  uk-margin-top uk-margin-remove-right" data-uk-grid >
+
+
+
+
+<div class="uk-card uk-card-default uk-box-shadow-hover-xlarge">
+    <div class="uk-card-header">
+        <div class="uk-grid-small uk-flex-middle" data-uk-grid>
+            <div class="uk-width-auto">
+                <img class="smallimg radius" src="http://young.scot/media/5781/istock_000041478802_xxxlarge.jpg" alt="User"/>
+            </div>
+            <div class="uk-width-expand">
+                <h3 class="uk-card-title uk-margin-remove-bottom">Party</h3>
+                <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">April 01, 2016</time></p>
+            </div>
+        </div>
+    </div>
+    <div class="uk-card-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+    </div>
+    <div class="uk-card-footer">
+        <div><a href="" class="uk-button uk-button-text">Respond</a>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<div class="uk-card uk-card-default uk-box-shadow-hover-xlarge">
+    <div class="uk-card-header">
+        <div class="uk-grid-small uk-flex-middle" data-uk-grid>
+            <div class="uk-width-auto">
+                <img class="smallimg radius" src="https://ak3.picdn.net/shutterstock/videos/9773921/thumb/9.jpg" alt="User"/>
+            </div>
+            <div class="uk-width-expand">
+                <h3 class="uk-card-title uk-margin-remove-bottom">Party</h3>
+                <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">April 01, 2016</time></p>
+            </div>
+        </div>
+    </div>
+    <div class="uk-card-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+    </div>
+    <div class="uk-card-footer">
+        <div><a href="" class="uk-button uk-button-text">Respond</a>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+<div class="uk-card uk-card-default uk-box-shadow-hover-xlarge">
+    <div class="uk-card-header">
+        <div class="uk-grid-small uk-flex-middle" data-uk-grid>
+            <div class="uk-width-auto">
+                <img class="smallimg radius" src="https://ak3.picdn.net/shutterstock/videos/9773921/thumb/9.jpg" alt="User">
+            </div>
+            <div class="uk-width-expand">
+                <h3 class="uk-card-title uk-margin-remove-bottom">Party</h3>
+                <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">April 01, 2016</time></p>
+            </div>
+        </div>
+    </div>
+    <div class="uk-card-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+    </div>
+    <div class="uk-card-footer">
+        <div><a href="" class="uk-button uk-button-text">Respond</a>
+        </div>
+    </div>
+</div>
+
+
+<div class="uk-card uk-card-default uk-box-shadow-hover-xlarge">
+    <div class="uk-card-header">
+        <div class="uk-grid-small uk-flex-middle" data-uk-grid>
+            <div class="uk-width-auto">
+                <img class="smallimg radius" src="https://ak3.picdn.net/shutterstock/videos/9773921/thumb/9.jpg" alt="User">
+            </div>
+            <div class="uk-width-expand">
+                <h3 class="uk-card-title uk-margin-remove-bottom">Party</h3>
+                <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">April 01, 2016</time></p>
+            </div>
+        </div>
+    </div>
+    <div class="uk-card-body">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+    </div>
+    <div class="uk-card-footer">
+        <div><a href="" class="uk-button uk-button-text">Respond</a>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+    </div>
+
+
+
+</li>
+
+<li>
+  <!--{/*mu posts*/}  -->
+    <div  class="uk-child-width-1-3@s uk-grid-small uk-grid-match uk-margin-remove" data-uk-grid data-uk-scrollspy="cls: uk-animation-fade; target: > div > .uk-card; delay: 500; repeat: true">
+
+
+
+ <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" src="https://ak3.picdn.net/shutterstock/videos/9773921/thumb/9.jpg" alt="User">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
+      <p><a href="">This is a link</a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+    </div>
+  </div>
+
+
+    </div>
+ </li>
+
+ <li>
+ <!--{/*messages*/}-->
+<div class="uk-margin-remove" data-uk-grid>
+    <div class="uk-width-3-5@l">
+        <div >Message
+
+
+
+        </div>
+    </div>
+    <div class="uk-width-1-5@l">
+        <div>My Messages</div>
+    </div>
+</div>
+</li>
+
+</ul>
+
+</div>
+
+
+
+
+
+
+
+
+ </div>
+
+
+
+
+</div>
+</template>
+
+<script>
+import Logo from '~components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container
+{
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.full{
+width: 100%;
+}
+
+.wave {
+  position: relative;
+  cursor: pointer;
+  display: inline-block;
+  overflow: hidden;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-tap-highlight-color: transparent;
+  vertical-align: middle;
+  z-index: 1;
+  transition: .3s ease-out;
+}
+
+.radius{
+    border-radius: 10rem;
+}
+
+.avatar{
+  width: 40%;
+  overflow: hidden;
+  position: relative;
+  margin: auto;
+ 
+}
+.avatar:after{
+  content: "";
+  padding-bottom: 100%;
+  display: block;
+}
+
+.avatar img{
+position: absolute;
+padding: 0rem;
+min-height: 100%;
+min-width: 100%;
+object-fit: cover;
+}
+
+mapst{
+  height: 78vh;
+
+}
+
+.smallimg{
+  width: 5em;
+  height: 5em;
+}
+
+.userimg{
+  width: 9em;
+  height: 7em;
+  
+}
+
+.radsuserimg{
+    border-radius: 10rem;
+}
+
+.sect{
+  padding-top: 1rem;
+}
+
+</style>
