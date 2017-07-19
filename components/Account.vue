77e998e2ce010fd02ea084e465888c846713b5c3
kh-ibragim <ibragim2010@list.ru>
2017-07-19 06:50:48 -0500

    components

---
+++ components/Account.vue
@@ -0,0 +1,105 @@
+<template>
+    <!--section-->
+    <div class="uk-section uk-section-muted uk-section uk-padding-remove-top uk-padding-remove-bottom ">
+        <div class="uk-card uk-card-muted uk-card-small uk-grid-collapse  uk-margin  uk-width-1-1 sect uk-margin-remove" data-uk-grid>
+            <div class="uk-card-media-right uk-overflow-hidden uk-width-1-1@s ">
+                <div class="avatar">
+                    <img  class="uk-border-circle" src="https://s-media-cache-ak0.pinimg.com/originals/6d/bd/ea/6dbdeacb3b7ad332be657c9ca64f4b54.jpg" alt=""/>
+                </div>
+            </div>
+        </div>
+        <!--edit profile   -->
+        <div>
+            <a class="uk-icon-button" uk-icon="icon: file-edit" title="Edit profile" uk-tooltip href="#modal-overflow" uk-toggle></a>
+
+            <div id="modal-overflow" uk-modal>
+                <div class="uk-modal-dialog">
+                    <button class="uk-modal-close-default" type="button" uk-close></button>
+                        <div class="uk-modal-header">
+                            <h2 class="uk-modal-title">Edit profile</h2>
+                        </div>
+                        <div class="uk-modal-body" uk-overflow-auto>
+
+                        <form >
+                        <div class="uk-margin">
+                        <div class="uk-inline full">
+                            <span class="uk-form-icon" uk-icon="icon: user"></span>
+                            <input id="icon_prefix" type="text" class="validate uk-input uk-form-width-large full radius" placeholder="Name" pattern="[a-zA-Z0-9]+" required >
+                            <label for="icon_prefix" data-error="Name должно содержать только символы из латинского алфавита и цифры"></label>
+                        </div>
+                        </div>
+
+                        <div class="uk-margin">
+                        <div class="uk-inline full">
+                            <span class="uk-form-icon" uk-icon="icon: lock"></span>
+                            <input id="password" type="password" pattern="(?=.*\d)\w+.{6,30}" class="validate uk-input uk-form-width-large full radius" placeholder="Password" required>
+                            <label for="password" data-error="Пароль должен состоять минимально из 6 латинских символов и включать, по крайней мере одну букву и одну цифру"></label>
+                        </div>
+                        </div>
+
+                        <div class="uk-margin">
+                        <div class="uk-inline full">
+                            <span class="uk-form-icon" uk-icon="icon: mail"></span>
+                            <input id="email" type="email" class="validate uk-input uk-form-width-large full radius" placeholder="Email" required >
+                            <label for="email" data-error="Введите корректно свой email"></label>
+                        </div>
+                        </div>
+
+                        <div class="uk-margin">
+                        <div class="uk-inline full">
+                            <span class="uk-form-icon" uk-icon="icon: pencil"></span>
+                        <input id="age" type="number" min="16" max="120" class="validate uk-input uk-form-width-large full radius" placeholder="Age" required>
+                        </div>
+                        </div>
+  
+                        <div class="uk-margin white radius">
+                        <div class="uk-form-controls full">
+                            <label><input class="uk-radio" type="radio" name="sex" value="true" checked> Man </label>
+                            <label><input class="uk-radio" type="radio" name="sex" value="false" >  Female</label>
+                        </div>
+                        </div>
+                        </form>
+                </div>
+
+                        <div class="uk-modal-footer uk-text-right">
+                            <button class="uk-button uk-button-default uk-modal-close" type="button">Cancel</button>
+                            <button class="uk-button uk-button-primary" type="button">Save</button>
+                        </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
+
+<style>
+full{
+width: 100%;
+}
+
+
+.radius{
+    border-radius: 10rem;
+}
+
+.avatar{
+  width: 17%;
+  overflow: hidden;
+  position: relative;
+  margin: auto;
+ 
+}
+.avatar:after{
+  content: "";
+  padding-bottom: 100%;
+  display: block;
+}
+
+.avatar img{
+position: absolute;
+padding: 0rem;
+min-height: 100%;
+min-width: 100%;
+object-fit: cover;
+}
+</style>
--- pages/profile.vue
+++ pages/profile.vue
@@ -1,57 +1,12 @@
 <template>
 <div>
 
-    <!--section-->
-    <div class="uk-section uk-section-muted uk-section uk-padding-remove-top uk-padding-remove-bottom ">
-        <div class="uk-card uk-card-muted uk-card-small uk-grid-collapse  uk-margin  uk-width-1-1 sect uk-margin-remove" data-uk-grid>
-            <div class="uk-card-media-right uk-overflow-hidden uk-width-1-5@s ">
-                <div class="avatar">
-                    <img  class="uk-border-circle" src="https://s-media-cache-ak0.pinimg.com/originals/6d/bd/ea/6dbdeacb3b7ad332be657c9ca64f4b54.jpg" alt=""/>
-                </div>
-            </div>
-        </div>
-     <!--edit profile   -->
-    <div>
-        <a class="uk-icon-button" uk-icon="icon: file-edit" title="Edit profile" uk-tooltip uk-toggle="target: #toggle-usage"></a>
-
-
-<form class="uk-form-horizontal " id="toggle-usage" hidden>
-
-
-    <div class="uk-margin">
-        <div class="uk-inline">
-            <span class="uk-form-icon" uk-icon="icon: user"></span>
-            <input id="icon_prefix" type="text" class="validate uk-input uk-form-width-medium radius" placeholder="Name" pattern="[a-zA-Z0-9]+" required >
-        </div>
-    </div>
-
-    <div class="uk-margin">
-        <div class="uk-inline">
-            <span class="uk-form-icon" uk-icon="icon: lock"></span>
-            <input id="age" type="number" min="16" max="120" class="validate uk-input uk-form-width-medium radius" placeholder="Age" required>
-        </div>
-    </div>
-
-    <div class="uk-margin">
-        <div class="uk-inline">
-          <button class="uk-button uk-button-muted uk-button-medium radius" type="submit"  value="Submit">Save
-          </button>
-        </div>
-    </div>
-
-</form>
-
-    </div>
+<Account />
 <!--
 {/*primary info*/}-->
         <div class="uk-section uk-section-default uk-padding-remove ">       
         <!--{/*navigation tabs*/}-->
-        <ul class="uk-flex-center uk-margin-remove uk-background-muted" data-uk-tab> 
-            <li class="uk-active"><a href="" title="Life" uk-tooltip class="uk-icon-link" uk-icon="icon: location; ratio: 1.5"></a></li>
-            <li><a href="" title="Meetings" uk-tooltip class="uk-icon-link" uk-icon="icon: world; ratio: 1.5"></a></li>
-            <li><a href="" title="My posts" uk-tooltip class="uk-icon-link" uk-icon="icon: home; ratio: 1.5"></a></li>
-            <li><a href="" title="Messages" uk-tooltip class="uk-icon-link" uk-icon="icon: mail; ratio: 1.5"></a></li>
-        </ul>
+        <Navigation />
 
         <ul class="uk-switcher uk-margin-remove uk-padding-remove">
             <li class="uk-active">
@@ -95,22 +50,42 @@
 
         <li>
         <!--{/*my posts*/}  -->
-            <div  class="uk-child-width-1-3@s uk-grid-small uk-grid-match uk-margin-remove" data-uk-grid data-uk-scrollspy="cls: uk-animation-fade; target: > div > .uk-card; delay: 500; repeat: true">
-            
-            <a class="uk-button uk-button-default" href="#modal-full" uk-toggle>Open</a>
 
-<div id="modal-full" class="uk-modal-full" uk-modal>
-    <div class="uk-modal-dialog">
-        <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
-        <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" uk-grid>
-            <div class="uk-background-cover" style="background-image: url('http://young.scot/media/5781/istock_000041478802_xxxlarge.jpg');" uk-height-viewport></div>
-            <div class="uk-padding-large">
-                <h1>Headline</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
+        <div class="uk-section uk-section-muted uk-padding-remove"> 
+            <a class="uk-icon-button uk-button-muted iplus" uk-icon="icon: plus; ratio: 1.5" title="Create Post" uk-tooltip href="#modal-overflow" uk-toggle></a>
         </div>
-    </div>
-</div>
+
+            <div  class="uk-child-width-1-2@s uk-child-width-1-4@m uk-grid-small uk-grid-match uk-margin-top uk-margin-left" data-uk-grid data-uk-scrollspy="cls: uk-animation-fade; target: > div > .uk-card; delay: 500; repeat: true">
+
+
+
+
+            <div>
+              <div class="uk-card uk-card-default">
+                <div class="uk-card-media-top">
+                    <img src="http://young.scot/media/5781/istock_000041478802_xxxlarge.jpg" alt="">
+                </div>
+                <div class="uk-card-body">
+                    <h3 class="uk-card-title">Media Top</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                </div>
+                <div class="uk-card-footer">
+                    <a class="uk-button uk-button-text" href="#modal-full" uk-toggle>More</a>
+                        <div id="modal-full" class="uk-modal-full" uk-modal>
+                            <div class="uk-modal-dialog">
+                                <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
+                                    <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" uk-grid>
+                                        <div class="uk-background-cover" style="background-image: url('http://young.scot/media/5781/istock_000041478802_xxxlarge.jpg');" uk-height-viewport></div>
+                                            <div class="uk-padding-large">
+                                                <h1>Headline</h1>
+                                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                </div>
+            </div>
 
 
             </div>
@@ -133,16 +108,18 @@
     </div>
 </div>
 
-
-</div>
 </template>
 
 <script>
 import Logo from '~components/Logo.vue'
+import Account from '~components/Account.vue'
+import Navigation from '~components/Navigation.vue'
 
 export default {
   components: {
-    Logo
+    Logo,
+    Account,
+    Navigation
   }
 }
 </script>
@@ -181,7 +158,7 @@ width: 100%;
 }
 
 .avatar{
-  width: 40%;
+  width: 17%;
   overflow: hidden;
   position: relative;
   margin: auto;
@@ -226,6 +203,11 @@ width: 5em;
 
 .sect{
   padding-top: 1rem;
+}
+.iplus{
+    margin-left: 50%; 
+    margin-top: 1%; 
+
 }
 
 </style>
